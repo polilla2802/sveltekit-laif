d8de35a0de41eb10d0d29adb0160973b1d1d9caa
Webmaster Tachuela <polilla2802@gmail.com>
2024-10-18 10:27:49 -0500

    Added styles + tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '$lib/styles/app.css';
+</script>
+
+<div class="layout bg-primaryBlue text-white">
+	<section class="container mx-auto px-4 py-4 flex-1 flex-container">
+		<div class="slot-content h-full">
+			<slot />
+		</div>
+	</section>
+</div>
+
+<style>
+</style>
